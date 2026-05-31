9b22826fa9dd5678a30bcc4f9a5e976bf3304123
Matthias Roscoe <matthias@ignitecreates.com>
2026-05-31 17:08:48 +0100

    improve: btn change

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -12,7 +12,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
             disabled={disabled || loading}
             className={cn(
                 'border-brown-dark inline-flex min-w-[200px] cursor-pointer items-center justify-center rounded-[100%] border',
-                'hover:bg-brown-dark/10 hover:border-brown-dark/10 ml-2 px-6 pt-4 pb-2 transition-colors',
+                'hover:bg-yellow hover:border-yellow ml-2 px-6 pt-4 pb-2 transition-colors',
                 'disabled:cursor-not-allowed disabled:opacity-60',
                 className
             )}
