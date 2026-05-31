fc8b43ae3b7a7768a479d93ab074c091449d9c86
Matthias Roscoe <matthias@ignitecreates.com>
2026-05-31 21:00:07 +0100

    fix: padding

--- src/components/rsvp-section.tsx
+++ src/components/rsvp-section.tsx
@@ -3,7 +3,7 @@ import { RsvpForm } from '@/components/rsvp-form'
 export function RsvpSection() {
     return (
         <section id="rsvp" className="section-wavy-top-cream bg-cream text-brown-dark">
-            <div className="container mx-auto max-w-xl px-5 pt-[140px] pb-16 md:px-8 md:pt-[240px] md:pb-24">
+            <div className="container mx-auto max-w-xl px-8 pt-[140px] pb-16 md:px-8 md:pt-[240px] md:pb-24">
                 <RsvpForm />
             </div>
         </section>
